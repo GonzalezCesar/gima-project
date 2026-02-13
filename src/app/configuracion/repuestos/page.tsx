c4551d4672a4b0316418c1ad5aa7c9853fb8fb7e
Naleskdcp <naleskadenniely@gmail.com>
2026-02-12 21:30:27 -0400

    Organizacion y comentarios en el codigo

--- src/app/configuracion/repuestos/page.tsx
+++ src/app/configuracion/repuestos/page.tsx
@@ -37,41 +37,6 @@ export default function RepuestosPage() {
             </h2>
           </div>
 
-          {/* CONTENEDOR DE BUSCADOR + ICONOS */}
-
-          {/* <div className="flex items-center gap-4">
-
-            <div className="relative font-archivo">
-              <Search
-                className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400"
-                size={18}
-              />
-
-              <input 
-                type="text"
-                placeholder="Buscar"
-                className="
-                  pl-12 pr-6 py-2.5
-                  rounded-full
-                  border-none
-                  shadow-sm
-                  w-72
-                  focus:ring-2 focus:ring-gima-blue
-                  bg-gima-light
-                  text-gima-navy
-                "
-              />
-            </div>
-
-            <button className="p-2.5 bg-gima-light rounded-xl shadow-sm border border-gray-100 hover:bg-white transition-all">
-              <Bell size={22} className="text-gima-navy" />
-            </button>
-
-            <button className="p-2.5 bg-gima-light rounded-xl shadow-sm border border-gray-100 hover:bg-white transition-all">
-              <User size={22} className="text-gima-navy" />
-            </button>
-          </div>  */}
-
         </header>
 
         {/* BOTÓN VOLVER */}
@@ -102,18 +67,7 @@ export default function RepuestosPage() {
               {/* BOTÓN NUEVO REPUESTO */}
               <button
                 onClick={() => setIsModalOpen(true)}
-                className="
-              bg-gima-blue
-              text-white
-              px-6 py-3
-              rounded-xl
-              flex items-center gap-2
-              hover:bg-[#0052cc]
-              transition-all
-              shadow-lg shadow-blue-100
-              font-microgramma
-              text-xs font-bold uppercase
-            ">
+                className="bg-gima-blue text-white px-6 py-3 rounded-xl flex items-center gap-2 hover:bg-[#0052cc] transition-all shadow-lg shadow-blue-100 font-microgramma text-xs font-bold uppercase">
                 <Plus size={20} strokeWidth={3} />
                 Nuevo repuesto
               </button>
@@ -125,18 +79,7 @@ export default function RepuestosPage() {
             <div className="w-full">
 
               {/* CABECERA DE LA TABLA */}
-              <div className="
-              grid grid-cols-6
-              bg-[#DAFAFE]
-              p-6
-              rounded-t-[30px]
-              text-gima-navy
-              font-microgramma
-              font-bold
-              text-[10px]
-              uppercase
-              tracking-[0.2em]
-            ">
+              <div className="grid grid-cols-6 bg-[#DAFAFE] p-6 rounded-t-[30px] text-gima-navy font-microgramma font-bold text-[10px] uppercase tracking-[0.2em]">
                 <span>Código</span>
                 <span>Descripción</span>
                 <span>Categoría</span>
@@ -152,16 +95,7 @@ export default function RepuestosPage() {
                 {repuestos.map((loc, index) => (
                   <div
                     key={index}
-                    className="
-                    grid grid-cols-6
-                    items-center
-                    p-6
-                    bg-white
-                    border-b border-gray-50
-                    last:border-0
-                    hover:bg-gray-50/50
-                  "
-                  >
+                    className=" grid grid-cols-6 items-center p-6 bg-white border-b border-gray-50 last:border-0 hover:bg-gray-50/50">
                     {/* ATRIBUTOS */}
                     <span className="text-sm text-gray-400 font-medium">{loc.codigo}</span>
                     <span className="text-sm font-bold text-gima-navy">{loc.descripcion}</span>
@@ -197,5 +131,3 @@ export default function RepuestosPage() {
     </div>
   );
 }
-
-//comentario de prueba para PR
